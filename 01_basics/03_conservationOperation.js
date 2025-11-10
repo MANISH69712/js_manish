06239d6c3b719a0030e03ed65c68c2a3993cf7fd
Manish Bastakoti <manishbastakoti0@gmail.com>
2025-11-10 09:05:16 +0000

    conservation

--- 01_basics/03_conservationOperation.js
+++ 01_basics/03_conservationOperation.js
@@ -1,1 +1,13 @@
-let score = 33
+//let score = 33
+
+let isLoggedIn = "manish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0=> false;
+// "" => false
+// "manish" => true
+
+let someNumber = 33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
